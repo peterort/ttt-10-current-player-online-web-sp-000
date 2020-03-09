d4e2c163a6e2818e557271b55cc42ab8596ea664
Peter Ort <peter@peterort.com>
2020-03-09 18:24:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,9 +11,9 @@ end
 
 
 def current_player(board)
-  if turn_count % 2 = 0
+  if turn_count % 2 == 0
     return "X"
-  else turn_count % 2 = 1
+  else turn_count % 2 == 1
     return "O"
 end
 end
