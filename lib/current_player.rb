7cad851b4cfd9b4f22c343c8cd93b18bf82f82c3
Peter Ort <peter@peterort.com>
2020-03-09 17:42:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,8 @@ def turn_count(board)
   counter = 0
   board.each do |board|
     if board == "X" || board == "O"
-      return counter += 1
-    else return counter +=0
+      counter += 1
+    else counter +=0
     end
 
 
